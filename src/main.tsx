a5c0e55eb1e1c79694cfc299b3eedf084d1a1730
Filament Site <build@filament.example.com>
2026-08-30 11:06:55 +0000

    Fix i18n import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { i18n } from './i18n'
+import i18n from './i18n'
 import { I18nextProvider } from 'react-i18next'
 
 createRoot(document.getElementById('root')!).render(
